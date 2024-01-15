435c8331527855416db70c2212d03f3653359dfb
Abdul Mohiz <fatizforever@gmail.com>
2024-01-16 00:20:31 +0500

    70%

---
+++ src/sections/CustomerReviews.jsx
@@ -0,0 +1,13 @@
+
+const customerReviews = () => {
+  return (
+    <section className="max-container ">
+      <h3 className="text-center font-palanquin text-4xl font-bold">What Our <span className="text-coral-red ">Customers</span>
+        Say?</h3>
+        <p className="info-text m-auto mt-4 max-w-lg text-center">Hear genuine stories from our satisfied customers about their exceptional experiences with us.</p>
+
+    </section>
+  )
+}
+
+export default customerReviews
