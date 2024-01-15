8dd1b4cc8b1184695875f1f7b37a27cd970070d2
Abdul Mohiz <fatizforever@gmail.com>
2024-01-16 01:38:44 +0500

    Complete

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,13 +1,32 @@
+import React from 'react';
+import ReviewCard from '../components/ReviewCard';
+import { reviews } from '../constants';
 
-const customerReviews = () => {
+const CustomerReviews = () => {
+  const { map } = Array;
+
   return (
-    <section className="max-container ">
-      <h3 className="text-center font-palanquin text-4xl font-bold">What Our <span className="text-coral-red ">Customers</span>
-        Say?</h3>
-        <p className="info-text m-auto mt-4 max-w-lg text-center">Hear genuine stories from our satisfied customers about their exceptional experiences with us.</p>
-
+    <section className="max-container">
+      <h3 className="text-center font-palanquin text-4xl font-bold">
+        What Our <span className="text-coral-red ">Customers</span> Say?
+      </h3>
+      <p className="info-text m-auto mt-4 max-w-lg text-center">
+        Hear genuine stories from our satisfied customers about their exceptional experiences with us.
+      </p>
+      <div className="mt-34 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+        {reviews.map((review) => (
+          <ReviewCard
+            key={review.customerName}
+            imgURL={review.imgURL}
+            alt={`${review.customerName} image`}
+            customerName={review.customerName}
+            rating={review.rating}
+            feedback={review.feedback}
+          />
+        ))}
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default customerReviews
+export default CustomerReviews;
